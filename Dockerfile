6b26d6daed13a254cf7e52f41ba4a92827e150ed
Gelmo <gelmo@outlook.com>
2020-02-11 21:25:18 -0600

    Initial Dockerfile and entrypoint

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM debian:buster-slim
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+    lib32gcc1=1:8.3.0-6 \
+    lib32stdc++6=8.3.0-6 \
+    wget=1.20.1-1.1 \
+    ca-certificates=20190110 \
+    rsync=3.1.3-6 \
+    unzip=6.0-23+deb10u1 \
+    tmux \
+    jq \
+    bc \
+    binutils \
+    ca-certificates \
+    util-linux \
+    python \
+    curl \
+    wget \
+    file \
+    tar \
+    bzip2 \
+    gzip \
+    unzip \
+    bsdmainutil \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN useradd -m warfork
+
+USER warfork
+
+RUN mkdir /home/warfork/Steam
+
+WORKDIR /home/warfork/Steam
+
+RUN wget -qO- https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar zxf -
+
+RUN /home/warfork/Steam/steamcmd.sh +quit
+
+WORKDIR /home/warfork
+
+RUN mkdir server
+
+COPY entrypoint.sh /usr/local/bin/
+
+CMD [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+set -x
+
+shopt -s extglob
+
+STEAM_DIR=$HOME/Steam
+SERVER_DIR=$HOME/server
+SERVER_INSTALLED_LOCK_FILE=$SERVER_DIR/installed.lock
+WF_DIR=$SERVER_DIR/Warfork.app/Contents/Resources/basewf
+WF_CUSTOM_CONFIGS_DIR="${WF_CUSTOM_CONFIGS_DIR-/var/wf}"
+
+installServer() {
+  echo '> Installing server ...'
+
+  $STEAM_DIR/steamcmd.sh \
+    +login anonymous \
+    +force_install_dir $SERVER_DIR \
+    +app_update 1136510 validate \
+    +quit
+
+  echo '> Done'
+
+  touch $SERVER_INSTALLED_LOCK_FILE
+
+}
+
+applyCustomConfigs() {
+  echo "> Checking for custom configs at \"$WF_CUSTOM_CONFIGS_DIR\" ..."
+
+  if [ -d "$WF_CUSTOM_CONFIGS_DIR" ]; then
+      echo '> Found custom configs, applying ...'
+      rsync -ri $WF_CUSTOM_CONFIGS_DIR/ $WF_DIR
+      echo '> Done'
+  else
+      echo '> No custom configs found to apply'
+  fi
+}
+
+startServer() {
+  echo '> Starting server ...'
+
+  optionalParameters=""
+
+  $SERVER_DIR/Warfork.app/Contents/Resources/wf_server.x86_64 \
+      $optionalParameters \
+      $WF_PARAMS
+
+}
+
+updateServer() {
+  echo '> Checking for server update ...'
+
+  $STEAM_DIR/steamcmd.sh \
+    +login anonymous \
+    +force_install_dir $HOME/server \
+    +app_update 1136510 \
+    +quit
+
+  echo '> Done'
+}
+
+if [ -f "$SERVER_INSTALLED_LOCK_FILE" ]; then
+  updateServer
+else
+  installServer
+fi
+
+applyCustomConfigs
+
+startServer
